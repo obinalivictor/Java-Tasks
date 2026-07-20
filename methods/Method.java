6af6130d54ab7b38672ef84d09127369cc128aa3
divine-mercyx <divineobinali9@gmail.com>
2026-07-20 19:42:46 +0100

    java assignment

---
+++ methods/Method.java
@@ -0,0 +1,32 @@
+	public class Method{
+
+	   public static int multiplication(int a,int b){
+	   
+	  int multi = a * b;
+	   
+	   return multi;
+	   
+	   }
+
+	public static int division(int a,int b){
+
+	  int divide = a /b;
+	  return divide;
+
+	}
+
+
+	  public static void main(String[] args){
+	  
+	  int   multiresult = multiplication(10,5);
+	    int divideresult = division(100,5);
+	  
+	  
+	  System.out.println(multiresult);
+	  System.out.println(divideresult);
+	  
+	  }
+
+
+
+	}
---
+++ methods/Methods.java
@@ -0,0 +1,69 @@
+
+
+public class Methods{
+   public static void main(String[] args){
+   
+   
+   Multiples();
+   Word();
+   
+   int numberresult = DoubleIt(10);
+   System.out.println(numberresult);
+   
+ int  averageresult = Average(34,23,32);
+   System.out.println(averageresult);
+   
+   System.out.println(Isrange(7,15,32));
+   
+   System.out.println(TrueFalse(-4));
+   
+   }
+   
+   public static void Word(){
+   
+   String message = "welcome to java";
+    System.out.println(message);
+   
+   }
+public static int DoubleIt(int a){
+
+    int number = a * 2;
+    return number;
+
+}
+ 
+ public static int Average( int a,int b,int c){
+ 
+int averagenumber = (a+b+c)/3;
+return averagenumber;
+
+ }
+ 
+ public static boolean Isrange(int n,int low,int high){
+   if(n >= low && n <= high){
+    return true;
+      
+    }
+    return false;  	
+}
+
+public static boolean TrueFalse(int n){
+
+    if(n < 0){
+  return true;
+}
+   return false;
+}	
+ public static void Multiples(int a){
+ 
+
+ for(int count= 1;count >= 5;count++)
+ 
+   a = a * count;
+ 
+ System.out.println(a);
+ }
+   
+   
+   
+}
